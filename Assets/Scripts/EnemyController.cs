3b2f9c92f343db1aba90004b30cabee5be5ba039
Anzle <robleewilliams3@gmail.com>
2021-02-21 00:52:16 -0800

    - Renamed Weapon to Buff - Removed the concept of Weak and Strong weapon PreFabs  - - Code now randomly assigns a number to the token and that value determies which sprite will be used dynamiclly

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,15 +6,18 @@ using UnityEngine.UI;
 public class EnemyController : MonoBehaviour
 {
     public float damage = 0f;
-    public float weapon = 0f;
-
-    public Text damageText;
-    public Text weaponText;
-
+    public float buff = 0f;
     public float moveSpeed = 5f;
+
+    public Sprite weakSprite;
+    public Sprite strongSprite;
+
+    Text damageText;
+    Text buffText;
     LayerMask stopsMovement;
-    
     Transform movePoint;
+    SpriteRenderer spriteRenderer;
+
 
     // Bryce Tracking
     GameObject bryce;
@@ -25,23 +28,30 @@ public class EnemyController : MonoBehaviour
     {
         stopsMovement = LayerMask.GetMask("StopMovement");
 
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
         movePoint = transform.GetChild(0);
         movePoint.SetParent(null);
 
         bryce = GameObject.Find("Bryce");
         brycesOldPosition = bryce.transform.position;
 
-        Transform weaponTransform = transform.Find("Stat Canvas/Buff");
-        if(weaponTransform != null) {
-            weapon = Mathf.Floor(Random.Range(1f, 5f));
-            weaponText = weaponTransform.GetComponent<Text>();
-            weaponText.text = weapon.ToString();
+        Transform buffTransform = transform.Find("Stat Canvas/Buff");
+        if(buffTransform != null) {
+            buff = Mathf.Floor(Random.Range(1f, 10f));
+
+            spriteRenderer.sprite = (buff > 5f)? strongSprite : weakSprite;
+
+            buffText = buffTransform.GetComponent<Text>();
+            buffText.text = buff.ToString();
         }
 
         Transform damageTransform = transform.Find("Stat Canvas/Health");
         if(damageTransform != null)
         {
-            damage = damage * Mathf.Floor(Random.Range(1f, 5f));
+            damage = damage * Mathf.Floor(Random.Range(1f, 10f));
+
+            spriteRenderer.sprite = (Mathf.Abs(damage) > 5f)? strongSprite : weakSprite;
             damageText = damageTransform.GetComponent<Text>();
             damageText.text = Mathf.Abs(damage).ToString();
         }
@@ -69,7 +79,7 @@ public class EnemyController : MonoBehaviour
         if(other.tag == "Player")
         {
             PlayerController pc = bryce.GetComponent<PlayerController>();
-            pc.calculateEffects(damage, weapon);
+            pc.calculateEffects(damage, buff);
 
             Destroy(movePoint.gameObject);
             Destroy(gameObject);
