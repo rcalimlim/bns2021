f3fa4501b4716bc303e083a45a989df3890872b0
Ross F. Calimlim <ross.calimlim@gmail.com>
2021-03-14 00:43:17 -0600

    add door sound transition

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -3,23 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-[RequireComponent(typeof(AudioSource))]
 public class SceneTransition : MonoBehaviour
 {
-    public string sceneToLoad;
-    AudioSource audioData;
-
-    void Start()
-    {
-        audioData = GetComponent<AudioSource>();
-    }
+    [SerializeField]
+    private string sceneToLoad;
+    [SerializeField]
+    private AudioClip transitionSoundEffect;
 
     public void OnTriggerEnter2D(Collider2D other)
     {
-	Debug.Log("collision detected");
         if (other.CompareTag("Player") && !other.isTrigger)
         {
-            audioData.Play(0);
+            SoundManager.Instance.PlayMusic(transitionSoundEffect);
             SceneManager.LoadScene(sceneToLoad);
         }
     }
