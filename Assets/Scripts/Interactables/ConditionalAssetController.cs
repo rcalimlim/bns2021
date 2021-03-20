9c4c7f38a5f795fafe9a1b4ae52ab3e7e49e66d5
Ross F. Calimlim <ross.calimlim@gmail.com>
2021-03-20 18:26:30 -0500

    fix up conditional asset controller

--- Assets/Scripts/Interactables/ConditionalAssetController.cs
+++ Assets/Scripts/Interactables/ConditionalAssetController.cs
@@ -4,28 +4,84 @@ using UnityEngine;
 
 public class ConditionalAssetController : MonoBehaviour
 {
+    [SerializeField] private bool isEnabled = true;
     [SerializeField] private bool isBlocking = true;
     [SerializeField] private string enableAfterTriggerFlag;
     [SerializeField] private string disableAfterTriggerFlag;
     [SerializeField] private string activatesTriggerFlagName;
+    private SpriteRenderer spriteRenderer;
 
     public bool IsBlocking { get { return isBlocking; } }
 
+    private void Awake()
+    {
+        SpriteRenderer sp = gameObject.GetComponent<SpriteRenderer>();
+        if (sp != null)
+        {
+            spriteRenderer = sp;
+        }
+
+        if (!IsEnabled())
+        {
+            Disable();
+        }
+    }
+
     private void Update()
     {
-        if (isBlocking == true && disableAfterTriggerFlag != "")
+        // needs to basically happen once
+        if (isEnabled == true && disableAfterTriggerFlag != "")
         {
             bool isDisabled = PlayerDataManager.Instance.GetTriggerFlag(disableAfterTriggerFlag);
-
             if (isDisabled)
             {
-                SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
                 if (spriteRenderer != null)
                 {
-                    spriteRenderer.enabled = false; 
+                    StartCoroutine(BlinkSpriteOff());
                 }
-                isBlocking = false;
+                Disable();
             }
         }
+    }
+    private bool IsEnabled()
+    {
+        bool isDisabled = false;
+        if (disableAfterTriggerFlag != "")
+        {
+            isDisabled = PlayerDataManager.Instance.GetTriggerFlag(disableAfterTriggerFlag);
+            if (isDisabled)
+            {
+                return false;
+            }
+        }
+
+        if (enableAfterTriggerFlag == "")
+        {
+            return true;
+        }
+
+        return PlayerDataManager.Instance.GetTriggerFlag(enableAfterTriggerFlag);
+    }
+
+    private void Disable()
+    {
+        isEnabled = false;
+        isBlocking = false;
+        spriteRenderer.enabled = false;
+    }
+
+    private IEnumerator BlinkSpriteOff()
+    {
+        for (int i = 0; i < 6; ++i)
+        {
+            if (spriteRenderer != null)
+            {
+                spriteRenderer.enabled = false;
+                yield return new WaitForSecondsRealtime(0.4f);
+                spriteRenderer.enabled = true;
+                yield return new WaitForSecondsRealtime(0.4f);
+            }
+        }
+        spriteRenderer.enabled = false;
     }
 }
