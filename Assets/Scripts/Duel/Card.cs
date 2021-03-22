b60eb5187e7bf92ce775a8de17b1fcd38d50734e
Anzle <robleewilliams3@gmail.com>
2021-03-22 16:47:09 -0700

    Updated Card. Started Deck

--- Assets/Scripts/Duel/Card.cs
+++ Assets/Scripts/Duel/Card.cs
@@ -9,12 +9,14 @@ public enum Attack
     Strike,
     Thrust
 }
+
 public enum Defense
 {
     Block,
     Dodge,
     Parry,
 }
+
 public class Card
 {
     private int strength;
@@ -34,12 +36,54 @@ public class Card
     public string Type{get => Enum.GetName(type.GetType(), type).Substring(0,1);}
     public string Name{get => $"{Type}{Strength}";}
     public string LongName{get => $"{LongType} {Strength}";}
+    public string CardClass{
+        get { return (type.GetType() == typeof(Attack))? "Attack" : "Defense";}
+    }
 
     /*
-     * 
+     * Helper Methods
     */
     public string AllCardInfo()
     {
         return $"Card Info: \n\tName: {Name}\n\tLong Name: {LongName}\n\tStrength: {Strength}\n\tType Name: {Type}\n\tLong Type Name: {LongType}";
+    }
+
+    private static Enum GetRandomType(Type enumType)
+    {
+        Array enums = Enum.GetValues(enumType);
+        return (Enum)enums.GetValue(UnityEngine.Random.Range(0, enums.Length));
+    }
+
+    /*
+     * Generators - Please use these to create Cards. 
+     * Note: typeStr is case sensitive
+    */
+    public static Card MakeAttackCard(int strength, string typeStr)
+    {
+        List<String> cardTypes = new List<string>(Enum.GetNames(typeof(Attack)));
+        if(strength > 0 && strength < 10 && cardTypes.Contains(typeStr))
+            return new Card(strength, (Attack) Enum.Parse(typeof(Attack), typeStr));
+        return null;
+    }
+
+    public static Card MakeDefenseCard(int strength, string typeStr)
+    {
+        List<String> cardTypes = new List<string>(Enum.GetNames(typeof(Defense)));
+        if(strength > 0 && strength < 10 && cardTypes.Contains(typeStr))
+            return new Card(strength, (Defense) Enum.Parse(typeof(Defense), typeStr));
+        return null;
+    }
+
+    // Random Generators
+    public static Card MakeRandomAttackCard()
+    {   
+        Enum type = GetRandomType(typeof(Attack));
+        return new Card(UnityEngine.Random.Range(1,10), type);
+    }
+
+    public static Card MakeRandomDefenseCard()
+    {
+        Enum type = GetRandomType(typeof(Defense));
+        return new Card(UnityEngine.Random.Range(1,10), type);
     }
 }
--- Assets/Scripts/Duel/Tests/CardTest.cs
+++ Assets/Scripts/Duel/Tests/CardTest.cs
@@ -7,7 +7,9 @@ public class CardTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        createCard();
+        //createCard();
+        //makeRandomCards();
+        //makeCard();
     }
 
 
@@ -18,6 +20,35 @@ public class CardTest : MonoBehaviour
 
         Card defenseCard = new Card(5, Defense.Parry);
         print("Defense Card - " + defenseCard.AllCardInfo());
-
     }
+
+    void makeRandomCards()
+    {
+        for(int i=0; i < 5; i++)
+            print("Random Attack Card:" + Card.MakeRandomAttackCard().AllCardInfo());
+
+        for(int i=0; i < 5; i++)
+            print("Random Defense Card:" + Card.MakeRandomDefenseCard().AllCardInfo());
+    }
+    void makeCard()
+    {
+        print($"Attempt MakeAttackCard (success): {Card.MakeAttackCard(1, "Lash")?.AllCardInfo()}");
+        print($"Attempt MakeAttackCard (success): {Card.MakeAttackCard(9, "Thrust")?.AllCardInfo()}");
+        print($"Attempt MakeAttackCard (success): {Card.MakeAttackCard(5, "Strike")?.AllCardInfo()}");
+        
+        print($"Attempt MakeAttackCard (failure): {Card.MakeAttackCard(-5, "Lash")?.AllCardInfo()}");
+        print($"Attempt MakeAttackCard (failure): {Card.MakeAttackCard(15, "Lash")?.AllCardInfo()}");
+        print($"Attempt MakeAttackCard (failure): {Card.MakeAttackCard(5, "Block")?.AllCardInfo()}");
+        print($"Attempt MakeAttackCard (failure): {Card.MakeAttackCard(5, "lash")?.AllCardInfo()}");
+
+        print($"Attempt MakeDefenseCard (success): {Card.MakeDefenseCard(1, "Block")?.AllCardInfo()}");
+        print($"Attempt MakeDefenseCard (success): {Card.MakeDefenseCard(9, "Dodge")?.AllCardInfo()}");
+        print($"Attempt MakeDefenseCard (success): {Card.MakeDefenseCard(5, "Parry")?.AllCardInfo()}");
+
+        print($"Attempt MakeDefenseCard (failure): {Card.MakeDefenseCard(0, "Parry")?.AllCardInfo()}");
+        print($"Attempt MakeDefenseCard (failure): {Card.MakeDefenseCard(10, "Dodge")?.AllCardInfo()}");
+        print($"Attempt MakeDefenseCard (failure): {Card.MakeDefenseCard(5, "parry")?.AllCardInfo()}");
+        print($"Attempt MakeDefenseCard (failure): {Card.MakeDefenseCard(1, "Lash")?.AllCardInfo()}");
+    }
+    
 }
