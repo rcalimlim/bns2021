866f1641508479740e9903b81ee5f3537d0b1c9a
Anzle <robleewilliams3@gmail.com>
2021-03-26 00:03:29 -0700

    75% of specials implemented - All one shot specials are done - 3 remain specials are stubbed out - Added portrait change to requip

--- Assets/ScriptableObjects/Scripts/Armor.cs
+++ Assets/ScriptableObjects/Scripts/Armor.cs
@@ -2,14 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu(fileName="New Armor", menuName="Item System/Armor")]
 public class Armor: EquipableItem
 {   
     [SerializeField]
     ArmorType type;
+
+    [SerializeField]
+    Sprite portrait;
+    
+    [SerializeField]
+    List<Sprite> extraPortraits;
+
     public override string Type
     {
         get => Enum.GetName(typeof(ArmorType), type); 
     }
+
+    public List<Sprite> ExtraPortraits {get => extraPortraits;}
+    public Sprite Portrait {get => portrait;}
 }
--- Assets/Scripts/Duel/DuelController.cs
+++ Assets/Scripts/Duel/DuelController.cs
@@ -33,6 +33,143 @@ public class DuelController : MonoBehaviour
 
     }
 
+    /**
+     * One Shot Specials
+    */
+    void BarbellMedic()
+    {
+        Debug.Log("Player used Barbell Medic!");
+        player.HP = 100;
+    }
+
+    void FullThrust()
+    {
+        Debug.Log("Player used Full Thust!");
+        List<Card> newHand = new List<Card>();
+        foreach(Card card in player.AttackHand)
+            newHand.Add(Card.MakeAttackCard(card.Strength, "Thrust"));
+        
+        player.ReplaceHand("Attack", newHand);
+    }
+
+    void Fahves()
+    {
+        Debug.Log("Player used FAHVES!");
+        List<Card> newPlayerAttackHand = new List<Card>();
+        List<Card> newPlayerDefenseHand = new List<Card>();
+        List<Card> newEnemyAttackHand = new List<Card>();
+        List<Card> newEnemyDefenseHand = new List<Card>();
+
+        // build new hands of the same cards but strength 5
+        for(int i = 0; i < 6; i++)
+        {
+            newPlayerAttackHand.Add(Card.MakeAttackCard(5,player.AttackHand[i].LongType));
+            newPlayerDefenseHand.Add(Card.MakeDefenseCard(5,player.DefenseHand[i].LongType));
+
+            newEnemyAttackHand.Add(Card.MakeAttackCard(5,enemy.AttackHand[i].LongType));
+            newEnemyDefenseHand.Add(Card.MakeDefenseCard(5,enemy.DefenseHand[i].LongType));
+        }
+            
+        // Replace the hands
+        player.ReplaceHand("Attack", newPlayerAttackHand);
+        player.ReplaceHand("Defense", newPlayerDefenseHand);
+
+        enemy.ReplaceHand("Attack", newEnemyAttackHand);
+        enemy.ReplaceHand("Defense", newEnemyDefenseHand);
+    }
+
+    void MangTomas()
+    {
+        Debug.Log("Player used M?ng Tom?s!");
+        int temp = player.HP;
+        player.HP = enemy.HP;
+        enemy.HP = temp;
+    }
+
+    void Chocododge()
+    {
+        Debug.Log("Player used Chocododge!");
+        List<Card> newEnemyAttackHand = new List<Card>();
+        List<Card> newEnemyDefenseHand = new List<Card>();
+
+        for(int i = 0; i < 6; i++)
+        {
+            newEnemyAttackHand.Add(Card.MakeAttackCard(enemy.AttackHand[i].Strength, "Strike"));
+            newEnemyDefenseHand.Add(Card.MakeDefenseCard(enemy.DefenseHand[i].Strength, "Dodge"));
+        }
+
+        enemy.ReplaceHand("Attack", newEnemyAttackHand);
+        enemy.ReplaceHand("Defense", newEnemyDefenseHand);
+    }
+
+    void ChineseRedVest()
+    {
+        Debug.Log("Player Used Chinese Red Vest!");
+        List<Card> newHand = new List<Card>();
+        foreach(Card card in player.DefenseHand)
+            newHand.Add(Card.MakeDefenseCard(9, card.LongType));
+        
+        player.ReplaceHand("Defense", newHand);
+    }
+
+    void SORD()
+    {
+        Debug.Log("Player Used SORD!");
+        List<Card> newHand = new List<Card>();
+        foreach(Card card in player.AttackHand)
+            newHand.Add(Card.MakeAttackCard(9, card.LongType));
+        
+        player.ReplaceHand("Attack", newHand);
+    }
+
+    void NO_theHolidaySpecial()
+    {
+        Debug.Log("Player Uesed NO!");
+        player.HP -= 30;
+    }
+
+    /**
+     * Effect Over Time Specials:
+    */
+    void ParryReposte()
+    {   
+        Debug.Log("Player Used Parry Reposte");
+        // Set this boolean?
+    }
+
+    void UlamEquilibrium()
+    {
+        Debug.Log("Player Used Ulam Equilibrium");
+        player.HP += 20;
+
+        // Set recurring boolean? 
+
+    }
+
+    void HowCanYouSee()
+    {
+        Debug.Log("Player Used How Can You See??");
+        // Set boolean
+    }
+
+    public void UseSpecial(Special special)
+    {
+        switch(special.name)
+        {
+            case "Barbell Medic": BarbellMedic(); break;
+            case "Full Thrust": FullThrust(); break;
+            case "Parry Reposte": ParryReposte(); break;
+            case "FAHVES": Fahves(); break;
+            case "Ulam Equilibrium": UlamEquilibrium(); break;
+            case "M?ng Tom?s": MangTomas(); break;
+            case "Chocododge": Chocododge(); break;
+            case "How Can You See??":HowCanYouSee(); break;
+            case "Chinese Red Vest": ChineseRedVest(); break;
+            case "SORD": SORD(); break;
+            case "NO": NO_theHolidaySpecial(); break;
+        }
+    }
+
     public void FakeTurn(Card played)
     {
         playerMove = played;
