bd399286eda23ee859fea0c28756165d5804f62d
Ross F. Calimlim <ross.calimlim@gmail.com>
2021-03-27 05:48:41 -0500

    ending setup

---
+++ Assets/VHEndingTransitionController.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VHEndingTransitionController : MonoBehaviour
+{
+    [SerializeField] private bool debugEnabled = false;
+    [SerializeField] private string enableAfterTriggerFlag;
+    [SerializeField] private string disableAfterTriggerFlag;
+    [SerializeField] private string activatesTriggerFlagName;
+    [SerializeField] private InventoryItem[] inventoryItems;
+    [SerializeField] private Inventory playerInventory;
+    [SerializeField] private bool autoEquipItems = false;
+    [SerializeField] private Dialog dialog;
+    [SerializeField] private string endingSceneName;
+
+    private bool IsEnabled()
+    {
+        if (debugEnabled)
+        {
+            return true;
+        }
+
+        bool isDisabled = false;
+        if (disableAfterTriggerFlag != "")
+        {
+            isDisabled = PlayerDataManager.Instance.GetTriggerFlag(disableAfterTriggerFlag);
+            if (isDisabled)
+            {
+                return false;
+            }
+        }
+
+        if (enableAfterTriggerFlag == "")
+        {
+            return true;
+        }
+
+        return PlayerDataManager.Instance.GetTriggerFlag(enableAfterTriggerFlag);
+    }
+
+    private void AddItemsToInventory()
+    {
+        foreach (InventoryItem inventoryItem in inventoryItems)
+        {
+            playerInventory.AddItem(inventoryItem.Item, inventoryItem.Qty);
+            if (autoEquipItems == true)
+            {
+                Debug.Log("autoequip true");
+                foreach (InventoryItem iItem in inventoryItems)
+                {
+                    playerInventory.Equip((EquipableItem)iItem.Item);
+                }
+            }
+        }
+    }
+
+    private IEnumerator InvokeActionsSync()
+    {
+        yield return new WaitForEndOfFrame();
+
+        if (dialog.Lines.Count > 0)
+        {
+            StartCoroutine(DialogManager.Instance.ShowDialog(dialog));
+        }
+
+        PlayerDataManager.Instance.SetTriggerFlag(activatesTriggerFlagName, true);
+
+        // go to ending scene
+        SceneManager.LoadScene(endingSceneName);
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (IsEnabled() && other.CompareTag("Player") && !other.isTrigger)
+        {
+            StartCoroutine(InvokeActionsSync());
+        }
+    }
+}
